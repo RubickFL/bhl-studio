d80bcfb5076fec213a593e22ad021869719cbe21
Sa1RaC <sa1rac.dedsec@gmail.com>
2019-02-09 00:12:07 +0400

    Tasks

---
+++ application/controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+	namespace BHLst\controllers;
+
+	use BHLst\controllers\Controller;
+
+
+	class HomeController extends Controller {
+
+		public function __constuct(){
+			parent::__constuct();
+		}
+
+		public function index(){
+			echo "Hello World";
+		}
+	}
+?>
